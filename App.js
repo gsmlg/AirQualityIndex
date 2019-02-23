e98839c5845c6736321f842f7a71bffe53baadde
gsmlg <me@gsmlg.org>
2019-02-23 15:44:29 +0800

    first AQI display

--- App.js
+++ App.js
@@ -2,10 +2,37 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+  state = {
+    aqi: '--'
+  }
+
+  componentDidMount() {
+    this.loadAQI();
+    this.loadInterval();
+  }
+
+  loadInterval() {
+    setTimeout(() => {
+      this.loadAQI();
+      this.loadInterval();
+    }, 15 * 60 * 1000);
+  }
+
+  loadAQI() {
+    fetch('https://api.waqi.info/feed/beijing/?token=323cc85735abe934588c86f66a7bea1ed595fc3f').then((resp) => {
+      return resp.json();
+    }).then((data) => {
+      this.setState({
+        aqi: data.data.aqi
+      });
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text style={styles.titleText}>Current AQI of BeiJing:</Text>
+        <Text style={styles.aqiText}>{this.state.aqi}</Text>
       </View>
     );
   }
@@ -18,4 +45,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titleText: {
+    fontSize: 24
+  },
+  aqiText: {
+    fontSize: 24,
+    color: 'red',
+  }
 });
